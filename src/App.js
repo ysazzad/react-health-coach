f0e863820223cddded21bd1bb1659c51f611d8f3
sazzad <hossensazzad1201@gmail.com>
2022-04-17 01:32:52 +0600

    component and file created

--- src/App.js
+++ src/App.js
@@ -1,9 +1,20 @@
 import logo from './logo.svg';
 import './App.css';
+import { Route, Routes } from 'react-router-dom';
+import About from './Pages/About/About';
+import Home from './Pages/Home/Home/Home';
+import Header from './Shared/Header/Header';
+import Footer from './Shared/Footer/Footer';
 
 function App() {
   return (
     <div className="App">
+      <Header></Header>
+      <Routes>
+        <Route path='/' element={<Home></Home>}></Route>
+        <Route path='/about' element={<About></About>}></Route>
+      </Routes>
+      <Footer></Footer>
 
     </div>
   );
---
+++ src/Pages/Home/Services/Services.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './Services.css'
+
+const Services = () => {
+    return (
+        <div>
+            <h1>Services</h1>
+
+        </div>
+    );
+};
+
+export default Services;
